559f3f193595ec409415c5e50aae6ae87cd31e9a
Joshua Mankelow <joshua@mankelow.org>
2022-09-27 14:38:05 +0100

    backup commit

--- src/bin/client/main.rs
+++ src/bin/client/main.rs
@@ -1,12 +1,23 @@
 use std::io::prelude::*;
+use std::str::from_utf8;
 use std::net::{TcpStream, SocketAddr};
 
 fn main() -> std::io::Result<()> {
     let addr = SocketAddr::from(([127, 0, 0, 1], 25565));
     let mut stream = TcpStream::connect(addr)?;
 
-    stream.write(&[1])?;
-    let a = stream.read(&mut [0; 128])?;
-    println!("{}", a);
+    stream.write(&[1, 2])?;
+
+    let mut data = [0 as u8; 1];
+    match stream.read_exact(&mut data) {
+        Ok(_) => {
+            let text = from_utf8(&data).unwrap();
+            println!("Data: {}", text);
+        }
+        Err(e) => {
+            println!("Error: {}", e);
+        }
+    }
+
     Ok(())
 }
--- src/bin/server/main.rs
+++ src/bin/server/main.rs
@@ -1,24 +1,41 @@
-use std::net::{TcpStream, TcpListener, SocketAddr};
+use std::net::{TcpStream, TcpListener, SocketAddr, Shutdown};
 use std::io::{Read, Write};
+use std::thread;
 
 fn main () {
-    let addr = SocketAddr::from(([127, 0, 0, 1], 25565));
+    let addr = SocketAddr::from(([0, 0, 0, 0], 25565));
     let listener = TcpListener::bind(addr).unwrap();
 
     let mut stream: TcpStream;
+
     for stream in listener.incoming() {
-        let mut data = [0 as u8; 128];
         match stream {
             Ok(mut s) => {
                 println!("New connection: {}", s.peer_addr().unwrap());
-                match s.read(&mut data) {
-                    Ok(d) => { println!("Data: {}", d) }
-                    Err(e) => { eprintln!("Error: {}", e) }
-                }
+                thread::spawn(move || {
+                    handle_client(&mut s)
+                });
             }
-            Err(_) => {
-                continue;
+            Err(e) => {
+                println!("Error: {}", e);
             }
         }
     }
+}
+
+fn handle_client(stream: &mut TcpStream) {
+    let mut data = [0 as u8; 50];
+
+    while match stream.read(&mut data) {
+        Ok(size) => {
+            println!("{:?}", data);
+            stream.write(&data[0..size]).unwrap();
+            true
+        },
+        Err(_) => {
+            println!("An error occured. Terminating connection with {}", stream.peer_addr().unwrap());
+            stream.shutdown(Shutdown::Both).unwrap();
+            false
+        }
+    } {}
 }
