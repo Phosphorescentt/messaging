6fbd985663d40f8119481047c18e16e7bb91e8ec
Joshua Mankelow <joshua@mankelow.org>
2022-10-03 22:21:30 +0100

    tried some more things and had a play. closer to something working

--- src/bin/client/main.rs
+++ src/bin/client/main.rs
@@ -5,19 +5,23 @@ use std::str::from_utf8;
 use std::net::{TcpStream, SocketAddr};
 use std::time::Duration;
 
+type TcpMutex = Arc<Mutex<TcpStream>>;
+type Buffer = (Vec<u8>, usize);
+
 fn get_input() -> String {
     let mut line = String::new();
     let _b = std::io::stdin().read_line(&mut line).unwrap();
     line
 }
 
-fn network_recv_thread(stream: Arc<Mutex<TcpStream>>) {
-    let mut data = [0_u8, 50];
+fn network_recv_thread(stream: TcpMutex) {
+    let (mut data, mut size): Buffer = (Vec::new(), 0);
     loop {
-        let d = stream.lock().unwrap().read(&mut data);
+        let d = stream.lock().unwrap().read_to_end(&mut data);
+        // println!("{:?}", data);
         match d {
-            Ok(_) => {
-                let text = from_utf8(&data).unwrap();
+            Ok(size) => {
+                let text = from_utf8(&data[0..size]).unwrap();
                 println!("Recieved: {}", text);
             }
             Err(_e) => {
@@ -27,22 +31,47 @@ fn network_recv_thread(stream: Arc<Mutex<TcpStream>>) {
     }
 }
 
-fn main() {
-    let stream = Arc::new(Mutex::new(TcpStream::connect("localhost:3333").unwrap()));
-    let _ = stream.lock().unwrap().set_nonblocking(true);
-    let stream2 = Arc::clone(&stream);
-    let network_handle = thread::spawn(move || {
-        network_recv_thread(stream2);
-    });
-
+// There's either something brokey here or something brokey in server/main.rs and I can't really
+// tell where lollers. Something is blocking somehwere and that's stopping messages from being sent
+// here. Is it possible that the reciever on the server is blocking the thread here? Alternatively
+// it just looks like the thread here is being blocked on writing to the stream somehow.
+fn network_send_thread(stream: TcpMutex) {
     loop {
         let input = get_input();
         if input.eq("exit\n") {
             break;
         } else {
             println!("Sending: {}", input);
-            let _ = stream.lock().unwrap().write(&input.into_bytes());
+            // let _ = stream.lock().unwrap().write(&input.into_bytes());
+            match stream.lock() {
+                Ok(mut s) => { 
+                    s.write(&input.into_bytes());
+                }
+                Err(e) => {
+                    eprintln!("Error: {}", e);
+                }
+            }
         }
     }
-    network_handle.join().expect("PANIC!!!!");
+}
+
+fn main() {
+    let stream = TcpStream::connect("localhost:3333").unwrap();
+    stream.set_nonblocking(true);
+    stream.set_read_timeout(Some(Duration::from_millis(1000)));
+    stream.set_write_timeout(Some(Duration::from_millis(1000)));
+
+    let stream = Arc::new(Mutex::new(stream));
+    let stream2 = Arc::clone(&stream);
+    let network_recv_handle = thread::spawn(move || {
+        network_recv_thread(stream2);
+    });
+
+    let stream2 = Arc::clone(&stream);
+    let network_send_handle = thread::spawn(move || {
+        network_send_thread(stream2);
+    });
+
+    network_recv_handle.join().expect("PANIC!!!!");
+    network_send_handle.join().expect("PANIC!!!!");
 }
--- src/bin/server/main.rs
+++ src/bin/server/main.rs
@@ -3,60 +3,71 @@ use std::sync::{Arc, Mutex, mpsc};
 use std::sync::mpsc::{Sender, Receiver};
 use std::net::{TcpListener, TcpStream, Shutdown};
 use std::io::{Read, Write};
+use std::time::Duration;
 
 type TcpMutex = Arc<Mutex<TcpStream>>;
-type Buffer = ([u8; 50], usize);
+type Buffer = (Vec<u8>, usize);
 
-fn handle_client_streams_mpsc(streams: Arc<Mutex<Vec<TcpMutex>>>, rx: Receiver<Buffer>) {
+fn distribute_messages(streams: Arc<Mutex<Vec<TcpMutex>>>, rx: Receiver<Buffer>) {
     loop {
         let r = rx.recv();
-        println!("Recieved data {:?}", r.unwrap());
-        let mut data = [0_u8; 50];
-        let mut size: usize = 0;
+        println!("Recieved data {:?}", r.as_ref().unwrap());
+        // let mut data = [0_u8; 50];
+        // let mut size: usize = 0;
+        let (mut data, mut size): Buffer = (Vec::new(), 0);
         match r {
             Ok(d) => (data, size) = d,
             Err(e) => eprintln!("Error: {}", e),
         }
 
+        // data = data.iter().map(|c| c.to_ascii_uppercase());
+        for char in data.iter_mut() {
+            char.to_ascii_uppercase();
+        }
+
         let streams_vec = streams.lock().unwrap();
         // println!("streams_vec: {:?}", streams_vec);
         for stream in streams_vec.iter() {
-            data = data.map(|c| c.to_ascii_uppercase());
-            println!("Sending '{:?}' to {}", data, stream.lock().unwrap().peer_addr().unwrap());
+            // println!("Sending '{:?}' to {}", data, stream.lock().unwrap().peer_addr().unwrap());
             let _ = stream.lock().unwrap().write(&data[0..size]);
-            println!("Done sending");
+            // println!("Done sending");
         }
     } 
 }
 
-fn handle_new_message(stream: TcpMutex, tx: Sender<Buffer>) {
-    let mut data = [0_u8; 50];
-    let size = stream.lock().unwrap().peek(&mut data).unwrap();
+fn client_handler(stream: TcpMutex, tx: Sender<Buffer>) {
+    // let mut data = [0_u8; 50];
+    // let size = stream.lock().unwrap().read_to_end(&mut data).unwrap();
 
-    let _ = tx.send((data, size));
+    let mut size: usize = 0;
+    let mut data: Vec<u8> = Vec::new();
+    loop {
+        let r = stream.lock().unwrap().read_to_end(&mut data);
+        match r {
+            Ok(s) => { 
+                size = s;
+                let _ = tx.send((data.clone(), size));
+            },
+            Err(e) => { eprintln!("Error: {}", e); },
+        }
+    }
+
 }
 
 fn main() {
     let streams: Arc<Mutex<Vec<TcpMutex>>> = Arc::new(Mutex::new(Vec::new()));
+    let mut client_handlers: Vec<thread::JoinHandle<()>> = Vec::new();
     let listener = TcpListener::bind("0.0.0.0:3333").unwrap();
     println!("Server listening on port 3333");
 
     let (tx, rx): (Sender<Buffer>, Receiver<Buffer>) = mpsc::channel();
     let streams2 = Arc::clone(&streams);
 
-    let handler_thread = thread::spawn(move || {
-        handle_client_streams_mpsc(Arc::clone(&streams2), rx);
+    let distribute_messages_thread = thread::spawn(move || {
+        distribute_messages(Arc::clone(&streams2), rx);
     });
 
-    // This means that messages are only processed every time a client connects to the server
-    // something needs to just be perma-listening to the stream to see if there's anything coming
-    // in and then if there is it needs to do something with that. Currently it seems liek there 
-    // are no issues with locking or blocking or anything like that, just the fact that we're only
-    // handling stuff when a new connection comes in.
-    //
-    // TL; DR the architecture of this part is completely borked.
-    // This for loop should set up new connections and then hand them off to another thread to do
-    // some networking magic rather than spawning a new thread for each new connection.
+    // Done this bit now
     for stream in listener.incoming() {
         match stream {
             Ok(s) => {
@@ -66,11 +77,17 @@ fn main() {
                 println!("Adding stream to streams");
                 streams.lock().unwrap().push(Arc::clone(&s));
 
-                let s2 = Arc::clone(&s);
                 let tx2 = tx.clone();
-                thread::spawn(move || {
-                    handle_new_message(s2, tx2);
+                let s2 = Arc::clone(&s);
+                s2.lock().unwrap().set_read_timeout(Some(Duration::from_millis(1000)));
+                // s2.lock().unwrap().set_write_timeout(Some(Duration::from_millis(1000)));
+                // s2.lock().unwrap().set_nonblocking(true);
+
+                let cur_thread = thread::spawn(move || {
+                    client_handler(s2, tx2);
                 });
+
+                client_handlers.push(cur_thread);
             }
             Err(e) => {
                 eprint!("Error: {}", e);
@@ -78,5 +95,9 @@ fn main() {
         }
     }
 
-    handler_thread.join().expect("PANIC!!!!!");
+    for handler in client_handlers {
+        handler.join().expect("See ya later clients.");
+    }
+
+    distribute_messages_thread.join().expect("PANIC!!!!!");
 }
